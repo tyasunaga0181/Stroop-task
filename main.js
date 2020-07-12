77256d03b0e861963e7f7b41209c9af5737a0800
tyasunaga0181 <67366372+tyasunaga0181@users.noreply.github.com>
2020-07-12 11:49:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -195,7 +195,7 @@ var main_stroop = {
   timeline: [{
   type: "html-keyboard-response",
   stimulus: jsPsych.timelineVariable('stimulus'),
-  post_trial_gap:2000,
+  post_trial_gap:1000,
   prompt: '<p>赤色ならR,  青色ならB,  緑色ならG,  黄色ならY</p>',
   choices: [82, 66, 89, 71],
   }],
